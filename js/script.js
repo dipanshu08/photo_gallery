898741da5358a69a440ae799fb7537375d37afcd
dipanshu <deepanshu07s@gmail.com>
2018-06-12 03:19:53 +0530

    Minor fixes

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@
 $('[data-fancybox="gallery"]').fancybox({
   loop: true,
   buttons: ['close'],
-  backFocus: false
+  backFocus: false,
 });
 
 const searchBox = document.getElementById('search');
 const images = document.getElementsByClassName('gallery')[0].children;
 
 searchBox.addEventListener('keyup', () => {
-  let query = searchBox.value;
+  let query = searchBox.value.toLowerCase();
 
   for (let i = 0; i < images.length; i++) {
     let img = images[i];
-    let imgAttribute = img.getAttribute('data-caption');
-    if (imgAttribute.indexOf(query) !== -1) {
+    let imgAttribute = img.getAttribute('data-caption').toLowerCase();
+    if (imgAttribute.indexOf(query) > -1) {
       img.style.display = 'inline-block';
     } else {
       img.style.display = 'none';
